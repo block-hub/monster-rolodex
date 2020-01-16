e9f071d4e7d0d1750c57e4d27c10f1ef1a4f6c99
PK <praveen@avya-tech.com>
2020-01-16 16:00:24 +0530

    Updates

---
+++ precache-manifest.cf3968f03ea7a62bbf13ab77fde5a455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb68d91febd270022aea40575b9b6e05",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "857c650e911343d63668",
+    "url": "/monsters-rolodex/static/css/main.d6da11d1.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "857c650e911343d63668",
+    "url": "/monsters-rolodex/static/js/main.2a8ebc6d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
